2d65ecf1155bc0860cfab897ce5b241ce3f2f2ff
Antonio Neto <antonio.queiroz@ifbaiano.edu.br>
2020-06-27 21:58:01 -0300

    created project

---
+++ src/test/java/com/antonio/mc/ProjetomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonio.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

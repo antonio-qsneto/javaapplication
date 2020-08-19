6a0174587e4a85ecdf53b3121ae5122d842324b0
Antonio Neto <prof.antonioqsneto@gmail.com>
2020-08-19 19:50:01 -0300

    create repository and  service for category

---
+++ src/main/java/com/antonio/mc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.antonio.mc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.antonio.mc.domain.Categoria;
+import com.antonio.mc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+
+}

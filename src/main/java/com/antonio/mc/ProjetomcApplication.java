a7b73d42d044c6dcebdee2662a4b3986ff34259b
Antonio Neto <prof.antonioqsneto@gmail.com>
2020-08-20 09:17:17 -0300

    create operation instantiation

--- src/main/java/com/antonio/mc/ProjetomcApplication.java
+++ src/main/java/com/antonio/mc/ProjetomcApplication.java
@@ -1,13 +1,33 @@
 package com.antonio.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.antonio.mc.domain.Categoria;
+import com.antonio.mc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetomcApplication {
+public class ProjetomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
